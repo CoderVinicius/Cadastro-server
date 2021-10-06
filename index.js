208530719ad63f81616114ba8243a9e032c65ab1
codervinicius <vinicaixx@gmail.com>
2021-10-06 18:41:53 -0300

    att

--- index.js
+++ index.js
@@ -9,29 +9,21 @@ const app = express();
 
 app.use(express.json());
 
-async function init() {
-  try {
-    const db = await connectToDb();
-    console.log("Conectado ao banco de dados!");
+const db = await connectToDb();
+console.log("Conectado ao banco de dados!");
 
-    app.use(
-      cors({
-        origin: "*",
-        methods: ["GET", "POST", "DELETE", "UPDATE", "PUT", "PATCH"],
-      })
-    );
-    app.use("/", userRouter);
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "DELETE", "UPDATE", "PUT", "PATCH"],
+  })
+);
+app.use("/", userRouter);
 
-    app.listen(process.env.PORT || 4000, function () {
-      console.log(
-        "Express server listening on port %d in %s mode",
-        this.address().port,
-        app.settings.env
-      );
-    });
-  } catch (err) {
-    console.log("Erro ao conectar ao banco de dados!", err);
-    process.exit(1);
-  }
-}
-init();
+app.listen(process.env.PORT || 4000, function () {
+  console.log(
+    "Express server listening on port %d in %s mode",
+    this.address().port,
+    app.settings.env
+  );
+});
